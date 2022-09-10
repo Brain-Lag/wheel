f07665cd4867b1956e0a47ef5745c94b34d1086d
levchenkolk <levchenko.levan@mail.ru>
2022-09-10 14:26:31 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+const int S = 1000 * 1000 * 100;
+double D = 80;
+double L = Math.PI * D;
+double MaxTurnover = 200000;
+double turnovers = S / L;
+if (turnovers < MaxTurnover)
+    Console.WriteLine("Запаса прочности хватит для преодоления данного расстояния");
+else
+    Console.WriteLine("Запаса прочности не хватит для преодоления данного расстояния");
